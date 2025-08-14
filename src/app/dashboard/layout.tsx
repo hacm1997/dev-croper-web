e3d5d99f731b8cae81d929a1da21d4962d069f25
hacm1997 <hacm1997@gmail.com>
2025-08-14 13:42:59 -0500

    feat: refactor middleware

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,16 @@
 "use client";
 import MainLayout from "@/components/organisms/dashboard/layout/main-layout";
-import { useSessionVerify } from "@/hooks/useSessionVerify";
+// import { useSessionVerify } from "@/hooks/useSessionVerify";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  useSessionVerify();
+  // useSessionVerify();
   return (
     <MainLayout>
-      <div className='pl-[20px] md:pl-[300px] flex flex-col gap-10 pt-[50px] w-[94%] h-auto'>
+      <div className="pl-[20px] md:pl-[300px] flex flex-col gap-10 pt-[50px] w-[94%] h-auto">
         {children}
       </div>
     </MainLayout>
